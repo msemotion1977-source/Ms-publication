b8d1de08e94872688b77a7df9ad47d7d26b75d75
msemotion1977-source <msemotion1977@gmail.com>
2026-08-15 22:48:41 +0200

    tiktok-oauth-start.js

--- dashboard/netlify/functions/tiktok-oauth-start.js
+++ dashboard/netlify/functions/tiktok-oauth-start.js
@@ -3,7 +3,7 @@ exports.handler = async (event) => {
   const clientKey = process.env.TIKTOK_CLIENT_KEY;
   const redirectUri = `${process.env.URL}/.netlify/functions/tiktok-oauth-callback`;
 
-  const scope = encodeURIComponent('video.publish,video.upload,user.info.basic');
+  const scope = encodeURIComponent('video.upload,user.info.basic');
   const authUrl =
     `https://www.tiktok.com/v2/auth/authorize/` +
     `?client_key=${clientKey}` +
